e4ba67b270ebae0827aa013cc80de32764e6187b
ganchau <gancchau@gmail.com>
2015-09-09 10:03:56 -0400

    updated with nsarray enumerateObjectsUsingBlock method

--- TwoDimensionalArray/ViewController.m
+++ TwoDimensionalArray/ViewController.m
@@ -11,6 +11,9 @@
 @interface ViewController ()
 
 @property (strong, nonatomic) NSArray *TwoDArray;
+@property (strong, nonatomic) NSString *object1;
+@property (strong, nonatomic) NSString *object2;
+@property (strong, nonatomic) NSString *object3;
 
 @end
 
@@ -20,46 +23,72 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+
+    [self initializeData];
+    [self searchUsingTwoForLoopsWithObject:self.object1];
+    [self searchUsingOneForLoopWithObject:self.object2];
+    [self searchUsingNoForLoopsWithObject:self.object3];
+}
+
+- (void)initializeData
+{
     self.TwoDArray = @[@[@"💡", @"🌜", @"👬", @"🌃"],
                        @[@"💬", @"💳", @"🎫", @"📅"],
                        @[@"🚊", @"🍕", @"🎾", @"🚶"],
                        @[@"🌈", @"🍦", @"🚌", @"🎭"]];
     
-    NSString *object1 = @"🎾";
-    NSString *object2 = @"🎭";
-    
-    [self searchUsingTwoLoopsWithObject:object1];
-    [self searchUsingOneLoopWithObject:object2];
+    self.object1 = @"🎾";
+    self.object2 = @"🎭";
+    self.object3 = @"👬";
 }
 
-- (void)searchUsingTwoLoopsWithObject:(NSString *)searchObj
+- (void)searchUsingTwoForLoopsWithObject:(NSString *)searchObj
 {
     for (NSUInteger i = 0; i < self.TwoDArray.count; i++) {
         NSArray *rowArray = self.TwoDArray[i];
         for (NSUInteger j = 0; j < rowArray.count; j++) {
             if ([self.TwoDArray[i][j] isEqualToString:searchObj]) {
-                NSLog(@"With two loops, %@ is at row: %lu col: %lu",
-                      searchObj, i + 1, j + 1);
+                NSLog(@"With two For loops, %@ is at row: %lu col: %lu", searchObj, i + 1, j + 1);
                 return;
-                // With two loops, 🎾 is at row: 3 col: 3
+                // With two For loops, 🎾 is at row: 3 col: 3
             }
         }
     }
     NSLog(@"WTF?!?");
 }
 
-- (void)searchUsingOneLoopWithObject:(NSString *)searchObj
+- (void)searchUsingOneForLoopWithObject:(NSString *)searchObj
 {
     for (NSUInteger i = 0; i < self.TwoDArray.count; i++) {
         NSArray *rowArray = self.TwoDArray[i];
         if ([rowArray containsObject:searchObj]) {
-            NSLog(@"With one loop, %@ is at row: %lu, col: %lu", searchObj,
-                  i + 1, [rowArray indexOfObjectIdenticalTo:searchObj] + 1);
+            NSLog(@"With one For loop, %@ is at row: %lu, col: %lu", searchObj, i + 1, [rowArray indexOfObjectIdenticalTo:searchObj] + 1);
             return;
-            // With one loop, 🎭 is at row: 4, col: 4
+            // With one For loop, 🎭 is at row: 4, col: 4
         }
     }
     NSLog(@"WTF?!?");
+}
+
+// Search through the array using NSArray's method enumerateObjectUsingBlock
+// If the obj is found, set *stop to YES and output the result
+// Return in this case will not work like in previous examples because we are dealing with a block
+// Therefore, the next command after the enumerate method will still execute
+// One way to remedy this is to check if we are at the last index and if we don't find the object, then print out the error message
+ 
+- (void)searchUsingNoForLoopsWithObject:(NSString *)searchObj
+{
+    [self.TwoDArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSArray *rowArray = (NSArray *)obj;
+        if ([rowArray containsObject:searchObj]) {
+            NSLog(@"With no For loops, %@ is at row: %lu col: %lu", searchObj, idx + 1, [rowArray indexOfObjectIdenticalTo:searchObj] + 1);
+            *stop = YES;
+            // With no For loops, 👬 is at row: 1 col: 3
+        }
+        if (self.TwoDArray.count == idx + 1) {
+            NSLog(@"WTF!?");
+        }
+    }];
 }
 
 @end
